dc77d6f7210164b261cc6cb609ef672768b9b85e
Alex Arana Rabanal <AARANAR@diresacajamarca.gob.pe>
2020-09-16 14:51:30 -0500

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   urlTalend:"http://190.116.36.121:8089/",
   //urlBackendSiscovid:"http://54.146.22.125/dir_siscovid/api/"
   urlBackendSiscovid:"http://hospitalvirtual.diresacajamarca.gob.pe/siscovid/api/",
-  urlBackendNode:"http://hospitalvirtual.diresacajamarca.gob.pe:8089/"
+  urlBackendNode:"http://hospitalvirtual.diresacajamarca.gob.pe:8001/"
 };
