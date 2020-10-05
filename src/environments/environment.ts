1a496cf9349ac11fb0552a93b798eb0c2c77769f
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-10-04 21:59:42 -0500

    agregare mapas de calor de covid de hospital virtual

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   urlBackendHospitalVirtual:"http://190.116.36.121:8088/",
   urlTalend:"http://190.116.36.121:8089/",
- //urlBackendSiscovid:"http://54.146.22.125/dir_siscovid/api/"
+ //urlBackendSiscovid:"http://54.146.22.125/dir_siscovid/api/",
   urlBackendSiscovid:"http://190.116.36.121/covid/api/",
   urlBackendNode:"http://localhost:8001/covid/"
 //  urlBackendNode:"http://190.116.36.121:8001/covid/"
