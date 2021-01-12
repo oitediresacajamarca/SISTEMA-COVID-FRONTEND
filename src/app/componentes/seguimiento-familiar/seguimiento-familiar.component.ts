53faa27c339ee40af0e2b4ecb492eba9c8de9de9
Daniel <dyland6@gmail.com>
2021-01-12 07:06:14 -0500

    seguimiento familiar diseño 2

--- src/app/componentes/seguimiento-familiar/seguimiento-familiar.component.ts
+++ src/app/componentes/seguimiento-familiar/seguimiento-familiar.component.ts
@@ -9,11 +9,15 @@ export class SeguimientoFamiliarComponent implements OnInit {
 
   nombrePersona : string //cambiar por objeto persona
   dni : string // same as above
+  evolucion : string[]
+
+
   constructor() { }
 
   ngOnInit(): void {
     this.nombrePersona = "PRUEBA PRUEBA PRUEBA"
     this.dni = "44552233"
+    this.evolucion = ["Evento 1","Evento 2","Evento 3", "Evento 4"]
   }
 
 }
