4dd1b0580f2f63c509976b8e6411ddb0c7c90972
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2021-04-28 03:21:37 -0500

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
   urlBackendNode:"http://190.116.36.121:8001/covid/",
   urlBackendAuth:"http://sir.diresacajamarca.gob.pe/dir_autenticacion/api/",
   siscovid_client_id : "diresa_seguimiento",
-  siscovid_cient_secret : "WrCcm69SZOVZpUpnYuq4"
+  siscovid_cient_secret : "WrCcm69SZOVZpUpnYuq4",
+  urlBackendNodeVacunas:"http://190.116.36.121:8001/vacunados/"
 };
